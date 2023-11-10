c38e9334ea2ccc1d5d4e54c2a7c9dc7c6f81ebc5
Ichivin <kevinblondiau@gmail.com>
2023-11-10 13:06:07 +0100

    add image preview and submit  condition

--- FrontEnd/assets/scripts/modale.js
+++ FrontEnd/assets/scripts/modale.js
@@ -3,6 +3,11 @@ function openModale(gallery) {
     modaleContainer.innerHTML = "";
     const modaleBackground = document.createElement("div");
     modaleBackground.className = "modale-background";
+    modaleBackground.addEventListener("click", (e) => {
+        if (e.target == modaleBackground) {
+            modaleContainer.innerHTML = "";
+        }
+    });
     modaleContainer.appendChild(modaleBackground);
 
     const modaleWhite = document.createElement("div");
@@ -84,6 +89,23 @@ function openModale(gallery) {
     inputFileLabel.setAttribute("for", "file");
     divAddImage.appendChild(inputFileLabel);
     inputFileLabel.innerText = "+ Ajouter photo";
+    inputFile.addEventListener("change", (e) => {
+        const imageDisplay = inputFile.files[0];
+        if (imageDisplay.size <= 4194304 && imageDisplay.type.split("/")[0] == "image") {
+            const fileReader = new FileReader();
+            fileReader.readAsDataURL(imageDisplay);
+            fileReader.addEventListener("load", (e) => {
+                imagePreview.innerHTML = `
+                <img alt="apercu" src=${e.target.result} />
+                `;
+                updateSubmitButton();
+            });
+        }
+    });
+
+    const imagePreview = document.createElement("div");
+    imagePreview.className = "modale-page2__image-preview";
+    divAddImage.appendChild(imagePreview);
 
     const tailleImage = document.createElement("p");
     tailleImage.className = "modale-page2__taille-image";
@@ -103,6 +125,9 @@ function openModale(gallery) {
     const modalePage2TextArea = document.createElement("input");
     modalePage2TextArea.className = "modale-page2__text-area";
     formFieldTitle.appendChild(modalePage2TextArea);
+    modalePage2TextArea.addEventListener("change", () => {
+        updateSubmitButton();
+    });
 
     const formFieldSelect = document.createElement("div");
     formFieldSelect.className = "form-field";
@@ -117,6 +142,9 @@ function openModale(gallery) {
     modalePage2Select.name = "category";
     modalePage2Select.className = "modale-page2__select";
     formFieldSelect.appendChild(modalePage2Select);
+    modalePage2Select.addEventListener("change", () => {
+        updateSubmitButton();
+    });
 
     fetch("http://localhost:5678/api/categories")
         .then((response) => response.json())
@@ -136,6 +164,7 @@ function openModale(gallery) {
     const submitButton = document.createElement("button");
     submitButton.className = "submit-button";
     submitButton.innerText = "Valider";
+    submitButton.disabled = true;
     form.appendChild(submitButton);
     form.addEventListener("submit", (e) => {
         e.preventDefault();
@@ -155,4 +184,25 @@ function openModale(gallery) {
                 }
             });
     });
+
+    function allFieldsAreFilled() {
+        const validImage =
+            inputFile.files[0] &&
+            inputFile.files[0].size <= 4194304 &&
+            inputFile.files[0].type.split("/")[0] == "image";
+        const validTitle = modalePage2TextArea.value.trim().length > 1;
+        const validCategory = modalePage2Select.value;
+        console.log(validImage, validTitle, validCategory);
+        return validImage && validTitle && validCategory;
+    }
+
+    function updateSubmitButton() {
+        if (allFieldsAreFilled()) {
+            submitButton.disabled = false;
+            submitButton.style.backgroundColor = "#1D6154";
+        } else {
+            submitButton.disabled = true;
+            submitButton.style.backgroundColor = null;
+        }
+    }
 }
